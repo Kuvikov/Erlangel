bb9757f08b75e0609038256a8b589e1e5dbd42b0
Kuvikov <pillarskp@gmail.com>
2024-11-25 20:40:55 +0300

    Compilate

--- hello.erl
+++ hello.erl
@@ -1,5 +1,15 @@
 -module(hello).
--export([start/0]).
+-export([add/2, hello/0, greet_and_two/2]).
 
-start() ->
+hello() ->
     io:format("Коллеги привет~n").
+
+add(X,Y) ->
+    X + Y.
+
+greet_and_two(A,B) -> 
+    hello(),
+    add(A,B).
+
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
