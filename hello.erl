314cb93274520c0ff98d12951faee13891e097c2
Kuvikov <pillarskp@gmail.com>
2024-11-26 12:10:52 +0300

    Version and conditions

--- hello.erl
+++ hello.erl
@@ -1,5 +1,7 @@
 -module(hello).
--export([add/2, hello/0, greet_and_two/2]).
+-vsn(2024).
+
+-export([add/2, hello/0, greet_and_two/2, greet/2]).
 
 hello() ->
     io:format("Коллеги привет~n").
@@ -10,3 +12,10 @@ add(X,Y) ->
 greet_and_two(A,B) -> 
     hello(),
     add(A,B).
+
+greet(male, Name) -> 
+    io:format("Здравствуй господин ~s!~n", [Name]);
+greet(female, Name) ->
+    io:format("Здравствуй госпожа ~s!~n", [Name]);
+greet(_, Name) ->
+    io:format("Здравтвуй ~s!~n", [Name]).
